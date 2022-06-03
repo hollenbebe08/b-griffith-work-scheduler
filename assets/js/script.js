a0e7a2c1b7ec1691605d224494de7f68bffe6f26
Betsy <betsy.griffith12@gmail.com>
2022-06-03 14:06:14 -0400

    allowed the save buttons to be clicked with one function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,31 @@
-saveBtnEl = document.getElementById("#eight");
-taskEl = $("task").value;
+// Variables
+var timeContainer = document.querySelector(".container");
+var tasks = $("#form-control").value;
 
-var saveTask = function() {
-    localStorage.getItem("task item", taskEl);
+//Time container to handle the clicking of buttons
+timeContainer.onclick = function() {
+    //to prevent default behavior of the button
+    event.preventDefault();
+    //stores elment as a variable
+    var element = event.target;
+    //if target element is a button
+    if(element.nodeName === "BUTTON") {
+        saveTask();
+    }
 };
 
-saveBtnEl.addEventListener("click", saveTask); 
+//saveTask function
+var saveTask = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var loadTask = function(){
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+};
+
+    //if nothing in localStorage, create a new object to track all task arrays
+    // if(!tasks) {
+    //     tasks = {
+    //     taskName: []
+    //     }
+    // }
