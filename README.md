2bf41f14a633051364495cbb12824398772381a6
Betsy <betsy.griffith12@gmail.com>
2022-06-04 22:21:13 -0400

    updated README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# WorkDay Scheduler
+
+## This app was created to make your day easier! The workday scheduler was designed to help out those who need assistance with keeping track of their daily tasks!
+
+## Website
+
+
+## Built with
+* HTML
+* CSS
+* JavaScript
+* Moment.js
+* Bootstrap
+* JQuery
+
+# Contribution
+Made by Betsy Griffith
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,18 @@ $("#currentDay").append(today);
 
 //set current hour
 var currentHour = moment().hour();
-console.log(currentHour);
+
+//checking to see if the current hour has passed or not
+var stateOfCurrentHour = function(hour){
+    if (hour < currentHour)
+    console.log(past);
+}
 
 // var setCurrentHours = function(current){
 //     console.log(current);
 
 //     var currentHour = Number.parseInt(current, 10);
 //     // console.log(currentHour);
-// }
-
-// to see if the time is current function
-// var currentTime = function(){
-//     if(moment().isAfter(now)){
-//         hourEl8.addClass("form-control future");
-//         console.log(hourEl8);
-//     }
 // }
 
 
@@ -39,27 +36,5 @@ console.log(currentHour);
 // var saveTask = function() {
 //     localStorage.setItem("tasks", JSON.stringify(tasks));
 // };
-
-// var loadTask = function(){
-//     tasks = JSON.parse(localStorage.getItem("tasks"));
-
-//     //if there are no tasks in storage...
-//     if(!tasks){
-//         tasks = {
-//             AM_8: [],
-//             AM_9: [],
-//             AM_10: [],
-//             AM_11: [],
-//             PM_12: [],
-//             PM_1: [],
-//             PM_2: [],
-//             PM_3: [],
-//             PM_4: [],
-//             PM_5: [],
-//         }
-//     }
-// };
-
-// loadTask();
 
 
