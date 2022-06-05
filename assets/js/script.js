0cfe131ef434e5e7b9bde2756a9ae49018c6a948
Betsy <betsy.griffith12@gmail.com>
2022-06-04 21:50:29 -0400

    set current hour with moment.js and added the hour class for styling to the hour divs in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,65 @@
-// Variables
-var timeContainer = document.querySelector(".container");
-tasks = {};
-
-
 //set current day
 var today = moment().format("MMMM Do YYYY");
-//append the current day to the jumbotron
 $("#currentDay").append(today);
 
+//set current hour
+var currentHour = moment().hour();
+console.log(currentHour);
 
-//get date from jumbotron
-var date = today
-//convert to moment object at 5:00pm and set current time
-var now = moment(date, "L").set("hour", 17);
+// var setCurrentHours = function(current){
+//     console.log(current);
+
+//     var currentHour = Number.parseInt(current, 10);
+//     // console.log(currentHour);
+// }
 
 // to see if the time is current function
-var currentTime = function(){
-    if(moment().isAfter(now)){
-        hourEl8.addClass("form-control future");
-        console.log(hourEl8);
-    }
-}
+// var currentTime = function(){
+//     if(moment().isAfter(now)){
+//         hourEl8.addClass("form-control future");
+//         console.log(hourEl8);
+//     }
+// }
 
 
 //Time container to handle the clicking of buttons
-timeContainer.onclick = function() {
-    //to prevent default behavior of the button
-    event.preventDefault();
-    //stores elment as a variable
-    var element = event.target;
-    //if target element is a button
-    if(element.nodeName === "BUTTON") {
-        saveTask();
-    }
-};
+// timeContainer.onclick = function() {
+//     //to prevent default behavior of the button
+//     event.preventDefault();
+//     //stores elment as a variable
+//     var element = event.target;
+//     //if target element is a button
+//     if(element.nodeName === "BUTTON") {
+//         saveTask();
+//     }
+// };
 
 
 // saveTask function
-var saveTask = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-};
+// var saveTask = function() {
+//     localStorage.setItem("tasks", JSON.stringify(tasks));
+// };
 
-var loadTask = function(){
-    tasks = JSON.parse(localStorage.getItem("tasks"));
+// var loadTask = function(){
+//     tasks = JSON.parse(localStorage.getItem("tasks"));
 
-    //if there are no tasks in storage...
-    if(!tasks){
-        tasks = {
-            AM_8: [],
-            AM_9: [],
-            AM_10: [],
-            AM_11: [],
-            PM_12: [],
-            PM_1: [],
-            PM_2: [],
-            PM_3: [],
-            PM_4: [],
-            PM_5: [],
-        }
-    }
-};
+//     //if there are no tasks in storage...
+//     if(!tasks){
+//         tasks = {
+//             AM_8: [],
+//             AM_9: [],
+//             AM_10: [],
+//             AM_11: [],
+//             PM_12: [],
+//             PM_1: [],
+//             PM_2: [],
+//             PM_3: [],
+//             PM_4: [],
+//             PM_5: [],
+//         }
+//     }
+// };
 
-loadTask();
+// loadTask();
 
 
