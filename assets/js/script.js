c9f19e20bdaec6aa9e2bf524413112a32bda3bcd
Betsy <betsy.griffith12@gmail.com>
2022-06-04 20:50:53 -0400

    reformatted the html to have everything in its own container and the hour, input, and button all in its own div with the class row as well. Added col m-9 for input to span it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,27 @@
 // Variables
 var timeContainer = document.querySelector(".container");
-var tasks = $("#form-control").value;
+tasks = {};
+
+
+//set current day
+var today = moment().format("MMMM Do YYYY");
+//append the current day to the jumbotron
+$("#currentDay").append(today);
+
+
+//get date from jumbotron
+var date = today
+//convert to moment object at 5:00pm and set current time
+var now = moment(date, "L").set("hour", 17);
+
+// to see if the time is current function
+var currentTime = function(){
+    if(moment().isAfter(now)){
+        hourEl8.addClass("form-control future");
+        console.log(hourEl8);
+    }
+}
+
 
 //Time container to handle the clicking of buttons
 timeContainer.onclick = function() {
@@ -14,18 +35,32 @@ timeContainer.onclick = function() {
     }
 };
 
-//saveTask function
+
+// saveTask function
 var saveTask = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
 var loadTask = function(){
     tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    //if there are no tasks in storage...
+    if(!tasks){
+        tasks = {
+            AM_8: [],
+            AM_9: [],
+            AM_10: [],
+            AM_11: [],
+            PM_12: [],
+            PM_1: [],
+            PM_2: [],
+            PM_3: [],
+            PM_4: [],
+            PM_5: [],
+        }
+    }
 };
 
-    //if nothing in localStorage, create a new object to track all task arrays
-    // if(!tasks) {
-    //     tasks = {
-    //     taskName: []
-    //     }
-    // }
+loadTask();
+
+
